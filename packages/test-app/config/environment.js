04415e8c5892aede7d18d2cb167c238260428e73
cloke <cory@me.com>
2022-08-01 17:29:33 -0400

    Attempt to get ghpages to work

--- packages/test-app/config/environment.js
+++ packages/test-app/config/environment.js
@@ -44,6 +44,8 @@ module.exports = function (environment) {
   }
 
   if (environment === 'production') {
+    ENV.rootURL = '/cb-ember-mobiledoc-editor'
+
     // here you can enable a production-specific feature
   }
 
