74c191cd0e0390d0f486bd46ee6d0ff7e733aee5
cloke <cory@me.com>
2022-08-03 11:29:07 -0400

    Add new files

---
+++ packages/test-app/app/components/card-example.js
@@ -0,0 +1,40 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+
+export default class CardExampleComponent extends Component {
+  card = {
+    name: 'kitten',
+    type: 'dom',
+    render() {
+      let el = document.createElement('figure');
+      el.innerHTML = `
+        <img src="http://placekitten.com/200/100">
+        <figcaption>Image of a kitten</figcaption>
+      `;
+      return el;
+    },
+  };
+
+  editor = null;
+  element = null;
+
+  get jsonContent() {
+    return JSON.stringify(this.content);
+  }
+
+  @action
+  editorDidLoad(editor, element) {
+    this.element = element.parentElement;
+    this.editor = editor;
+
+    this.editor.cards.push(this.card);
+    this.element.querySelector('.insert-card').addEventListener(
+      'click',
+      (evt) => {
+        editor.insertCard('kitten');
+        evt.preventDefault();
+      },
+      true
+    );
+  }
+}
